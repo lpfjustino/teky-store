e5a0d8a38144fdd8b0223c3c072a7ec75bbbf8c9
luis <lpfjustino@gmail.com>
2019-10-14 06:50:40 -0300

    Updates

---
+++ precache-manifest.c59b7b6b75f588f6af50b326fca6cf00.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c42bb408b5d7cdf44da0c6ca90c9b2",
+    "url": "/teky-store/index.html"
+  },
+  {
+    "revision": "38e8a9502ef12ff8bdbb",
+    "url": "/teky-store/static/css/2.3ad05019.chunk.css"
+  },
+  {
+    "revision": "e5eb0d2a133652a68ebd",
+    "url": "/teky-store/static/css/main.c3688337.chunk.css"
+  },
+  {
+    "revision": "38e8a9502ef12ff8bdbb",
+    "url": "/teky-store/static/js/2.7c489e7d.chunk.js"
+  },
+  {
+    "revision": "e5eb0d2a133652a68ebd",
+    "url": "/teky-store/static/js/main.1d7ca675.chunk.js"
+  },
+  {
+    "revision": "f3faaca9a08956ac914f",
+    "url": "/teky-store/static/js/runtime-main.401ffca0.js"
+  },
+  {
+    "revision": "ef69b2793d80479d8f4536b69fd08da5",
+    "url": "/teky-store/static/media/Teky_LogoApp_Color.ef69b279.svg"
+  },
+  {
+    "revision": "99cbe65c97ab0f391db4d8704c44a7d7",
+    "url": "/teky-store/static/media/Teky_LogoIcon_Color.99cbe65c.svg"
+  },
+  {
+    "revision": "8a26a12b04a90f6ecd9078626a980d6c",
+    "url": "/teky-store/static/media/Teky_LogoNoIcon_Mono_white.8a26a12b.svg"
+  },
+  {
+    "revision": "758597147502dce698f6c8ec3dffb3cc",
+    "url": "/teky-store/static/media/btn_AppleStore.75859714.svg"
+  },
+  {
+    "revision": "14ee73fdedb40d25e2dcb8a031aa6841",
+    "url": "/teky-store/static/media/btn_GooglePlay.14ee73fd.svg"
+  },
+  {
+    "revision": "ae22510aa039a8bb73dcbe6fcc48a222",
+    "url": "/teky-store/static/media/btn_Social_Facebook.ae22510a.svg"
+  },
+  {
+    "revision": "290af4ba544e59cbe26100748379b9b8",
+    "url": "/teky-store/static/media/btn_Social_Instagram.290af4ba.svg"
+  },
+  {
+    "revision": "e20f612f1559dcad3d72dc90208aacfa",
+    "url": "/teky-store/static/media/btn_Social_Whatsapp.e20f612f.svg"
+  },
+  {
+    "revision": "02b857d87fed9201f150cb87843f134b",
+    "url": "/teky-store/static/media/btn_Social_Youtube.02b857d8.svg"
+  },
+  {
+    "revision": "ef487e2103feea4506a99210f3c95b39",
+    "url": "/teky-store/static/media/btn_WindowsStore.ef487e21.svg"
+  },
+  {
+    "revision": "329f25931804e6f5fa612986317a8461",
+    "url": "/teky-store/static/media/ic_Apps.329f2593.svg"
+  },
+  {
+    "revision": "017f34fa388c3553671b3e168324cc4f",
+    "url": "/teky-store/static/media/ic_menu.017f34fa.svg"
+  },
+  {
+    "revision": "7ac2082ae570d1845daf0ff8ddcf9825",
+    "url": "/teky-store/static/media/ic_produtos_abrasivos.7ac2082a.svg"
+  },
+  {
+    "revision": "0beaf245aab11f78781b536d9b9ca95e",
+    "url": "/teky-store/static/media/ic_produtos_correias.0beaf245.svg"
+  },
+  {
+    "revision": "7203ca650271f7b40e7aaebf74bdbcdf",
+    "url": "/teky-store/static/media/ic_produtos_eletricos.7203ca65.svg"
+  },
+  {
+    "revision": "e5f5ae814805e9bc4849d4f4eb0ca3c0",
+    "url": "/teky-store/static/media/ic_produtos_epi.e5f5ae81.svg"
+  },
+  {
+    "revision": "17995b28d90942326230713674063a6c",
+    "url": "/teky-store/static/media/ic_produtos_ferramentas.17995b28.svg"
+  },
+  {
+    "revision": "869001b38189fc3c19f7e0dd774f53c4",
+    "url": "/teky-store/static/media/ic_produtos_fixacao.869001b3.svg"
+  },
+  {
+    "revision": "8cb6e5f0635ff17edea98d9176c17ea5",
+    "url": "/teky-store/static/media/ic_produtos_hidraulicos.8cb6e5f0.svg"
+  },
+  {
+    "revision": "e2503073807f44d8bf05ad346ec24287",
+    "url": "/teky-store/static/media/ic_produtos_motores.e2503073.svg"
+  },
+  {
+    "revision": "cf386ec71c2430966a8de9239dda1b99",
+    "url": "/teky-store/static/media/ic_produtos_oleos.cf386ec7.svg"
+  },
+  {
+    "revision": "5e4ff709670ce65d0639b9f68b2d1cb8",
+    "url": "/teky-store/static/media/ic_produtos_rolamentos.5e4ff709.svg"
+  },
+  {
+    "revision": "8c6989994b0768899cb78123ebb2e4e6",
+    "url": "/teky-store/static/media/ic_search.8c698999.svg"
+  },
+  {
+    "revision": "91e3510a09172b49ffc06ac0362c1f74",
+    "url": "/teky-store/static/media/ic_teky_sniper.91e3510a.svg"
+  },
+  {
+    "revision": "12f891827bf3db50f7ae605525b43892",
+    "url": "/teky-store/static/media/ic_teky_sniper_gray.12f89182.svg"
+  },
+  {
+    "revision": "9c66f0dbf4051e99f0508cbf32d16d1d",
+    "url": "/teky-store/static/media/local_grocery_store.9c66f0db.svg"
+  },
+  {
+    "revision": "061ebd7e613c1326a11adf4530ef404e",
+    "url": "/teky-store/static/media/pgt_AmericanExpress.061ebd7e.svg"
+  },
+  {
+    "revision": "5f0333599eb5fea9b3aa4e47d31275de",
+    "url": "/teky-store/static/media/pgt_Boleto.5f033359.svg"
+  },
+  {
+    "revision": "dd8620087478ad56ede41670b0add1d5",
+    "url": "/teky-store/static/media/pgt_Dinners.dd862008.svg"
+  },
+  {
+    "revision": "7545c205aa737948abac698780698536",
+    "url": "/teky-store/static/media/pgt_Elo.7545c205.svg"
+  },
+  {
+    "revision": "5938bb1f60853dd8906c7053894aeede",
+    "url": "/teky-store/static/media/pgt_Hipercard.5938bb1f.svg"
+  },
+  {
+    "revision": "b9f5a093a0a1e1c1436ad0a68ccde6fa",
+    "url": "/teky-store/static/media/pgt_Maestro.b9f5a093.svg"
+  },
+  {
+    "revision": "817da25e912a765f379d3b122f8cc212",
+    "url": "/teky-store/static/media/pgt_Mastercard.817da25e.svg"
+  },
+  {
+    "revision": "66357873faaf56f82f524a8a101906b0",
+    "url": "/teky-store/static/media/pgt_Visa.66357873.svg"
+  },
+  {
+    "revision": "0eaa643bad1e1b8c384437122c8c7402",
+    "url": "/teky-store/static/media/sec_LetsEncrypt_Square.0eaa643b.svg"
+  },
+  {
+    "revision": "8210837721bace72f1a2edb343124a80",
+    "url": "/teky-store/static/media/sec_PCI_DSS.82108377.svg"
+  },
+  {
+    "revision": "2e1632caa2bc43a503530025b3a25167",
+    "url": "/teky-store/static/media/sec_SiteBlindado.2e1632ca.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teky-store/precache-manifest.21cf5754a196e0b3877e47490a86438b.js"
+  "/teky-store/precache-manifest.c59b7b6b75f588f6af50b326fca6cf00.js"
 );
 
 self.addEventListener('message', (event) => {
